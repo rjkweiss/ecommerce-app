d8dabe81050fc0c4a1b697ee286c7be76c4e8cde
Joyce Kimojino-Weiss <jkimojinoweiss@gmail.com>
2025-03-19 19:37:16 -0500

    MongoDB database connection configuration + cleaning up default project setup code

---
+++ src/app/api/test-db/route.js
@@ -0,0 +1,11 @@
+import dbConnect from "@/lib/mongodb";
+
+export const GET = async () => {
+    try {
+        await dbConnect();
+        return new Response(JSON.stringify({ message: 'Connected to MongoDB'}), { status: 200 });
+    } catch (error) {
+        console.log(error);
+        return new Response(JSON.stringify({error: 'Database Connection failed'}), { status: 500 });
+    }
+};
